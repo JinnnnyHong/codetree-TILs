164076ebbce4572700482fca72f9d604a84861af
Hong Jieun <121860486+JinnnnyHong@users.noreply.github.com>
2024-07-14 14:39:52 +0900

    Title: 배열의 특정 위치 찾기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240714/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240714/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -2,8 +2,11 @@ k = 0
 l = []
 lst = list(map(int, input().split()))
 for i in lst:
-    if i % 2 == 0:
+    if i % 2 == 0 and i % 3 != 0:
         k += i
-    elif i % 3 == 0:
+    elif i % 2 != 0 and i % 3 == 0:
+        l.append(i)
+    elif i %2 ==0 and i % 3 == 0:
+        k += i
         l.append(i)
 print(k, round(sum(l)/len(l), 1))
