4357d3c192ecaaf6455e9501eda92faa81d89b26
Hong Jieun <121860486+JinnnnyHong@users.noreply.github.com>
2024-07-21 18:52:06 +0900

    Title: 나눗셈의 나머지, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240721/나눗셈의 나머지/remainder-of-division.py
+++ 240721/나눗셈의 나머지/remainder-of-division.py
@@ -1,9 +1,9 @@
 a, b = map(int, input().split())
-counts = [0] * 100
+counts = [0] * b
 while True:
-    a = a // b
     c = a % b
     counts[c] += 1
+    a = a // b
     if a < 1:
         break
 squares_sum = sum(x**2 for x in counts)
