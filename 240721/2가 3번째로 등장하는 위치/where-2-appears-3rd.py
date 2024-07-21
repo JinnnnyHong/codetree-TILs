b9acdadff39ffe5890d93a03f03592ae1da26af3
Hong Jieun <121860486+JinnnnyHong@users.noreply.github.com>
2024-07-21 20:22:24 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240721/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -2,7 +2,8 @@ n = int(input())
 lst = list(map(int, input().split()))
 cnt = 0
 for i in range(len(lst)):
-    lst[i] == 2
-    cnt += 1
-    if cnt == 3:
-        print(i+1)
+    if lst[i] == 2:
+        cnt += 1
+        if cnt == 3:
+            print(i + 1)
+            break
