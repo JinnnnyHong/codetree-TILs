ba16304e3f871f2c5d92f0a1da0017758f705761
Hong Jieun <121860486+JinnnnyHong@users.noreply.github.com>
2024-07-28 18:33:33 +0900

    Title: 배열로 사각형 만들기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240728/배열로 사각형 만들기/print-array-in-rectangle.py
@@ -0,0 +1,13 @@
+n = 5
+a = [[0 for _ in range(n)] for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == 0:
+            a[i][j] = 1
+        else:
+            a[i][j] = a[i-1][j] + a[i][j-1]
+
+for i in range(n):
+    for j in range(n):
+        print(a[i][j], end = ' ')
+    print()
