4446a6f186395039fe10b5cf4184bf1cc2ff1895
Hong Jieun <121860486+JinnnnyHong@users.noreply.github.com>
2024-07-28 17:58:41 +0900

    Title: 대각선으로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240728/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 240728/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -2,16 +2,18 @@ n, m = map(int, input().split())
 a = [[0 for _ in range(m)] for _ in range(n)]
 count = 1
 
-for s in range(n + m - 1):
-    if s % 2 == 0:
+for diag in range(n + m - 1):
+    if diag % 2 == 0:
+        # 짝수 대각선: 왼쪽에서 오른쪽, 위에서 아래
         for row in range(n):
-            col = s - row
+            col = diag - row
             if 0 <= col < m:
                 a[row][col] = count
                 count += 1
     else:
+        # 홀수 대각선: 위에서 아래, 왼쪽에서 오른쪽
         for col in range(m):
-            row = s - col
+            row = diag - col
             if 0 <= row < n:
                 a[col][row] = count
                 count += 1
