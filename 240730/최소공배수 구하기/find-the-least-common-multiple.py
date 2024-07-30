50164b7899c530f30f1ecaf01358f015681dc9ac
Hong Jieun <121860486+JinnnnyHong@users.noreply.github.com>
2024-07-30 18:31:20 +0900

    Title: 최소공배수 구하기, Time: 226ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240730/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,16 +1,10 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
 def lcm(a, b):
-    num = 1
-    k = max(a, b)
-    l = []
-    for i in range(k):
-        if a % num == 0 and b % num == 0:
-            l.append(num)
-            num += 1
-        else:
-            num += 1
-    for j in range(1,len(l)):
-        l[j] = l[j]*l[j-1]
-    print(l[-1])
+    return abs(a * b) // gcd(a, b)
 
 n, m = map(int, input().split())
-lcm(n,m)
+print(lcm(n, m))
