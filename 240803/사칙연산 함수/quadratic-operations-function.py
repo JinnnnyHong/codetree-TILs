5ec694029e6b4179d872fc5fa2e8b9087919dbfc
Hong Jieun <121860486+JinnnnyHong@users.noreply.github.com>
2024-08-03 17:37:31 +0900

    Title: 사칙연산 함수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240803/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,24 @@
+def four(a, o, c):
+    operator = ['+', '-', '/', '*']
+    if o in operator:
+        if o == '+':
+            result = a+c
+            print(f"{a} + {c} = {result}")
+        elif o == "-":
+            result = a-c
+            print(f"{a} - {c} = {result}")
+        elif o == '/':
+            result = a+c
+            result = int(result)
+            print(f"{a} / {c} = {result}")
+        else:
+            result = a*c
+            print(f"{a} * {c} = {result}")
+    else:
+        print("False")
+
+s,v,t = input().split()
+
+s = int(s)
+t = int(t)
+four(s,v,t)
